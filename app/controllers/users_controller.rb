7b15ae5db268f051041edbfb9280f2915b758a9c
Willem van der Jagt <wkjagt@gmail.com>
2015-02-04 20:26:09 -0500

    check user secret when chrome extension requests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,24 +1,20 @@
 class UsersController < ApplicationController
 
-  before_action :set_headers, :validate_secret
+  before_action :set_headers
   respond_to :json, :html
 
   def show
     @user = GithubUser.find_by(github_username: params[:user])
-    respond_with @user.prs_to_review
+
+    return head :not_found unless @user
+    return head :forbidden unless params["application-secret"] == @user.secret
+
+    render json: @user.prs_to_review.to_json(include: :github_user)
   end
 
   private
 
   def set_headers
     headers['Access-Control-Allow-Origin'] = '*'
-  end
-
-  def validate_secret
-    head :forbidden unless params["application-secret"] == secret_for_username(params["user"])
-  end
-
-  def secret_for_username(username)
-    Digest::MD5.hexdigest("replace_with_secret-#{username}")
   end
 end
