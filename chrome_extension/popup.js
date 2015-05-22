a707d6d32d23c8df7209aee4ea71954734914bac
Willem van der Jagt <wkjagt@gmail.com>
2015-05-22 14:09:46 +0000

    wip

--- chrome_extension/popup.js
+++
@@ -1,5 +0,0 @@
-chrome.runtime.sendMessage(null, { action : 'get_prs' }, function(response){
-  var prs = response['prs'];
-  var tpl = _.template($('script#pr_template').html());
-  $('#status').html(tpl({ prs: prs }));
-});
