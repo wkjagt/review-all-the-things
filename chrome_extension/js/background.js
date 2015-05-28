4a33779ced4046d625b021c54a4e6894a9493c86
Willem van der Jagt <wkjagt@gmail.com>
2015-05-28 18:23:01 +0000

    wip

--- chrome_extension/js/background.js
+++ chrome_extension/js/background.js
@@ -1,13 +1,9 @@
 var onMsg = function(msg, sender, sendResponse) {
-  getPullRequests(sendResponse);
-}
-
-var getPullRequests = function(callback) {
   $.ajax({
     url: 'https://review-all-the-things.herokuapp.com/users/'+localStorage['github-username'],
     async : false,
     data: { 'application-secret': localStorage['application-secret'] },
-    success: callback
+    success: sendResponse
   });
 }
 chrome.runtime.onMessage.addListener(onMsg);
