2852dce641d0bf46f48fc27be7e7453670130f9c
Willem van der Jagt <wkjagt@gmail.com>
2015-05-28 19:48:31 +0000

    templates

--- chrome_extension/js/contentscript.js
+++ chrome_extension/js/contentscript.js
@@ -5,47 +5,23 @@ function table_header() {
 }
 
 function pr_row(pr, user_name) {
-
   var my_pr = user_name == pr['github_user']['github_username'];
-  return render('pr_row.html', { "pr" : pr, "user_name" : user_name, "my_pr" : my_pr });
-
-  return ''+
-  '<li class="table-list-item pr-row" '+ (my_pr ? 'style="display:none"' : '')+'>'+
-  '  <div class="table-list-cell table-list-cell-type"></div>'+
-  '  <div class="table-list-cell issue-title">'+
-  '    <a href="'+ pr['repository']['url']+'" class="issue-title-link issue-nwo-link">'+ pr['repository']['name']+'</a>'+
-  '    <a href="'+ pr['url'] +'" class="issue-title-link">'+ pr['title'] +'</a>'+
-  '    <span style="padding-left:5px;">';
-
-  for(var i in pr['reviews']) {
-    var review = pr['reviews'][i],
-        user = review['github_username'],
+  var review_attributes = function(review) {
+    var user = review['github_username'],
         my_review = user_name == review['github_username'];
-
     if(review['status'] == 'to_review') {
-      var status_class = 'octicon octicon-sync',
-          tooltip = my_review ? "you haven't approved this PR yet" : (review['github_username'] + " hasn't approved this PR yet");
+      return {
+        'status_class' : 'octicon octicon-sync',
+        'tooltip' : my_review ? "you haven't approved this PR yet" : (review['github_username'] + " hasn't approved this PR yet")
+      }
     } else {
-      var status_class = 'octicon text-success octicon-check',
-          tooltip = my_review ? "you have approved this PR": (review['github_username'] + " has approved this PR");
+      return {
+        'status_class' : 'octicon text-success octicon-check',
+        'tooltip' : my_review ? "you have approved this PR": (review['github_username'] + " has approved this PR")
+      }
     }
-    row += ''+
-      '<span class="tooltipped tooltipped-e" aria-label="'+ tooltip +'">'+
-      '  <span class="'+status_class+'"></span>'+
-      '</span>'
   }
-  return row+
-  '    </span>'+
-  '    <div class="issue-meta">'+
-  '      <span class="issue-meta-section opened-by">'+
-  '        opened <time class="timeago" datetime="'+ pr['created_at'] +'">'+ pr['created_at'] +'</time>'+(my_pr ? '' : ' by <a>' + pr['github_user']['github_username']) + '</a>' +
-  '      </span>'+
-  '    </div>'+
-  '  </div>'+
-  '  <div></div>'+
-  '  <div class="table-list-cell issue-comments"><a></a></div>'+
-  '</li>';
-
+  return render('pr_row.html', { "pr" : pr, "user_name" : user_name, "my_pr" : my_pr, 'review_attributes' : review_attributes });
 }
 
 link.click(function(e){
